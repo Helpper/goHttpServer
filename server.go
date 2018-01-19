75386a44ff430f4c930e503016313e0108c52afe
周佳欢 <jiahuanzhou@zhoujiahuandeMacBook-Pro-3.local>
2018-01-19 11:37:08 +0800

    add listfiles

--- server.go
+++ server.go
@@ -5,8 +5,13 @@ import(
 	//"github.com/labstack/echo/middleware"
 	"html/template"
 	"io"
+	"path/filepath"
+	"os"
+	"strconv"
 	//"fmt"
 )
+
+var directory string = "./file"
 
 type User struct{
 	Username string `json:"username" form:"username" query:"username"`
@@ -19,13 +24,18 @@ type Template struct{
 
 type Title struct{
 	IndexTitle string
-	ListFileTiTle string
+	ListFileTitle string
 	UploadTitle string
+}
+
+type File struct{
+	Name string `json:"name"`
+	Size string `json:"size"`
 }
 
 var d = Title{
 	IndexTitle: "fileServer",
-	ListFileTiTle: "listFile",
+	ListFileTitle: "listFile",
 	UploadTitle: "uploadfile",
 }
 
@@ -44,14 +54,33 @@ func listFile(c echo.Context) error{
 	return c.Render(http.StatusOK, "listFile", d)
 }
 
+func listAllFiles(c echo.Context) error{
+	listOfFiles := make([]File, 0)
+	filepath.Walk(directory, func(path string, f os.FileInfo, err error) error{
+		if f == nil{
+			return err
+		}
+		if f.IsDir(){
+			return nil
+		}
+		var t File
+		t.Name = f.Name()
+		t.Size = strconv.FormatInt(f.Size()/1024, 10)
+		listOfFiles = append(listOfFiles, t)
+		return nil
+	})
+	return c.JSON(http.StatusOK, listOfFiles)
+}
+
 func main(){
 	e := echo.New()
-	e.Static("/", "static")
+	e.Static("/static", "assets")
 	t := &Template{
 		templates : template.Must(template.ParseGlob("views/*.html")),
 	}
 	e.Renderer = t
 	e.GET("/index", index)
-	e.GET("/files", listFile)
+	e.GET("/listFile", listFile)
+	e.GET("/files", listAllFiles)
 	e.Logger.Fatal(e.Start(":8000"))
 }
